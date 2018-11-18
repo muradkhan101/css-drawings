b558e0ac95ae7c58f5a45ba044cb970c96a51fa3
Murad Khan <mkhan@sbmcorp.com>
2018-11-18 08:11:29 -0800

    Added base logic for node drawing

--- graph-search/node.js
+++ graph-search/node.js
@@ -2,7 +2,7 @@ class Node {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-
+        this.size = 3;
         this.connections = [];
 
         this.heuristic = -Infinity;
@@ -12,12 +12,26 @@ class Node {
 
     draw(visited) {
         if (!visited.includes(this)) {
-            stroke(255);
+            this.updateFill();
+            this.updateStroke();
+            ellipse(this.x, this.y, this.size);
             visited.push(this);
             this.connections.forEach(edge => {
                 line(this.x, this.y, edge.x, edge.y);
                 edge.draw(visited);
-            })
+                this.updateStroke();
+            });
         }
+    }
+    updateFill() {
+        if (this.connections.length) fill(255);
+        else fill(200, 0, 0);
+    }
+    updateStroke() {
+        if (this.connections.length) stroke(255);
+        else stroke(200, 0, 0);
+    }
+    connect(node) {
+        this.connections.push(node);
     }
 }
